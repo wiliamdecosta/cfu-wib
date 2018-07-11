fadf74a867079ac5efb607b758c30654d818c343
saepudin06 <saepudin06@gmail.com>
2018-07-11 13:22:25 +0700

    ''

--- application/models/parameter/Tblm_costcenter.php
+++ application/models/parameter/Tblm_costcenter.php
@@ -11,10 +11,20 @@ class Tblm_costcenter extends Abstract_model {
     public $alias           = "";
 
     public $fields          = array(
-
+                                'costcenterid_pk'      => array('pkey' => true, 'type' => 'int', 'nullable' => true, 'unique' => true, 'display' => 'COSTCENTERID PK'),
+                                'cccode'         => array('nullable' => false, 'type' => 'str', 'unique' => false, 'display' => 'Cost Center Code'),
+                                'ccname'         => array('nullable' =>  false, 'type' => 'str', 'unique' => false, 'display' => 'Cost Center Name'),
+                                'parentcode'         => array('nullable' =>  true, 'type' => 'str', 'unique' => false, 'display' => 'Parent Code'),
+                                'orglevel'         => array('nullable' =>  true, 'type' => 'int', 'unique' => false, 'display' => 'Level'),
+                                'sourcetype'         => array('nullable' =>  false, 'type' => 'str', 'unique' => false, 'display' => 'Source'),
+                                'description'         => array('nullable' => true, 'type' => 'str', 'unique' => false, 'display' => 'DESCRIPTION'),
+                                'creationdate'         => array('nullable' => true, 'type' => 'str', 'unique' => false, 'display' => 'Creation Date'),
+                                'createdby'             => array('nullable' => true, 'type' => 'str', 'unique' => false, 'display' => 'Created By'),
+                                'updateddate'          => array('nullable' => true, 'type' => 'str', 'unique' => false, 'display' => 'Updated Date'),
+                                'updatedby'            => array('nullable' => true, 'type' => 'str', 'unique' => false, 'display' => 'Updated By')
                             );
 
-    public $selectClause    = "costcenterid_pk, cccode kode, ccname nama, parentcode, orglevel, sourcetype, description, creationdate, createdby, updateddate, updatedby";
+    public $selectClause    = "costcenterid_pk, cccode kode, ccname nama, parentcode, orglevel, sourcetype, description, creationdate, createdby, updateddate, updatedby, cccode, ccname";
     public $fromClause      = "tblm_costcenter";
 
     public $refs            = array();
@@ -31,19 +41,39 @@ class Tblm_costcenter extends Abstract_model {
         if($this->actionType == 'CREATE') {
             //do something
             // example :
+
+            unset($this->record['creationdate']);
+            unset($this->record['updateddate']);
+
             $this->db->set('creationdate',"sysdate",false);
             $this->record['createdby'] = $userdata['user_name'];
             $this->db->set('updateddate',"sysdate",false);
             $this->record['updatedby'] = $userdata['user_name'];
 
+            if(empty($this->record['orglevel'])) {
+                $this->db->set('orglevel',"null",false);
+                unset($this->record['orglevel']);
+            }
+
+          
             $this->record[$this->pkey] = $this->generate_id($this->table, $this->pkey);
 
         }else {
             //do something
             //example:
+            unset($this->record['creationdate']);
+            unset($this->record['createdby']);
+            unset($this->record['updateddate']);
 
             $this->db->set('updateddate',"sysdate",false);
             $this->record['updatedby'] = $userdata['user_name'];
+
+            if(empty($this->record['orglevel'])) {
+                $this->db->set('orglevel',"null",false);
+                unset($this->record['orglevel']);
+            }
+
+
         }
         return true;
     }
---
+++ application/views/parameter/tblm_costcenter.php
@@ -0,0 +1,384 @@
+<!-- breadcrumb -->
+<div class="page-bar">
+    <ul class="page-breadcrumb">
+        <li>
+            <a href="<?php base_url(); ?>">Home</a>
+            <i class="fa fa-circle"></i>
+        </li>
+        <li>
+            <a href="#">Parameter</a>
+            <i class="fa fa-circle"></i>
+        </li>
+         <li>
+            <a href="#">Master</a>
+            <i class="fa fa-circle"></i>
+        </li>
+        <li>
+            <span>Telin Cost Center</span>
+        </li>
+    </ul>
+</div>
+<!-- end breadcrumb -->
+<div class="space-4"></div>
+<div class="row">
+    <div class="col-xs-12">
+            <div class="space-4"></div>
+            <div class="row">
+                <label class="control-label col-md-2">Pencarian :</label>
+                <div class="col-md-3">
+                    <div class="input-group">
+                        <div class="input-group">
+                        <input id="i_search" type="text" class="FormElement form-control">
+                        <span class="input-group-btn">
+                            <button class="btn btn-success" type="button" id="btn-search" onclick="showData()">Cari</button>
+                        </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12">
+                   <table id="grid-table"></table>
+                   <div id="grid-pager"></div>
+                </div>
+            </div>
+    </div>
+</div>
+
+<?php $this->load->view('lov/lov_tblm_costcenter'); ?>
+<script>
+    function showLOVPCostCenter(id, code) {
+        modal_lov_tblm_costcenter_show(id, code);
+    }
+
+    function clearCostCenter() {
+        $('#form_costcenterid_pk').val('');
+        $('#form_costcentercode').val('');
+    }
+</script>
+<script>
+    function showData(){
+        var i_search = $('#i_search').val();
+
+        jQuery("#grid-table").jqGrid('setGridParam',{
+            url: '<?php echo WS_JQGRID."parameter.tblm_costcenter_controller/read"; ?>',
+            postData: {
+                i_search : i_search
+            }
+        });
+        $("#grid-table").trigger("reloadGrid");
+    }
+</script>
+
+<script>
+
+    jQuery(function($) {
+        var grid_selector = "#grid-table";
+        var pager_selector = "#grid-pager";
+
+        jQuery("#grid-table").jqGrid({
+            url: '<?php echo WS_JQGRID."parameter.tblm_costcenter_controller/crud"; ?>',
+            datatype: "json",
+            mtype: "POST",
+            colModel: [
+                {label: 'ID', name: 'costcenterid_pk', key: true, width: 5, sorttype: 'number', editable: true, hidden: true},
+                {label: 'Cost Center Code',name: 'cccode',width: 200, align: "left",editable: true,
+                    editoptions: {
+                        size: 30,
+                        maxlength:64
+                    },
+                    editrules: {required: true}
+                },
+                {label: 'Cost Center Name',name: 'ccname',width: 200, align: "left",editable: true,
+                    editoptions: {
+                        size: 30,
+                        maxlength:64
+                    },
+                    editrules: {required: true}
+                },
+                {label: 'Parent Code',
+                    name: 'parentcode',
+                    width: 200,
+                    sortable: true,
+                    editable: true,
+                    hidden: true,
+                    editrules: {edithidden: true, required:false},
+                    edittype: 'custom',
+                    editoptions: {
+                        "custom_element":function( value  , options) {
+                            var elm = $('<span></span>');
+
+                            // give the editor time to initialize
+                            setTimeout( function() {
+                                elm.append('<input id="form_costcenterid_pk" type="text"  style="display:none">'+
+                                        '<input id="form_costcentercode" readonly type="text" class="FormElement form-control" placeholder="Choose PL Item Group">'+
+                                        '<button class="btn btn-success" type="button" onclick="showLOVPCostCenter(\'form_costcenterid_pk\',\'form_costcentercode\')">'+
+                                        '   <span class="fa fa-search bigger-110"></span>'+
+                                        '</button>');
+                                $("#form_costcentercode").val(value);
+                                elm.parent().removeClass('jqgrid-required');
+                            }, 100);
+
+                            return elm;
+                        },
+                        "custom_value":function( element, oper, gridval) {
+
+                            if(oper === 'get') {
+                                return $("#form_costcentercode").val();
+                            } else if( oper === 'set') {
+                                $("#form_costcentercode").val(gridval);
+                                var gridId = this.id;
+                                // give the editor time to set display
+                                setTimeout(function(){
+                                    var selectedRowId = $("#"+gridId).jqGrid ('getGridParam', 'selrow');
+                                    if(selectedRowId != null) {
+                                        var code_display = $("#"+gridId).jqGrid('getCell', selectedRowId, 'parentcode');
+                                        $("#form_costcentercode").val( code_display );
+                                    }
+                                },100);
+                            }
+                        }
+                    }
+                },
+                {label: 'Level',name: 'orglevel',width: 150, align: "left", hidden:true, editable: true, number:true,
+                    editoptions: {
+                        size: 15,
+                        maxlength:255,
+                        dataInit: function(element) {
+                            $(element).keypress(function(e){
+                                 if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+                                    return false;
+                                 }
+                            });
+                        }
+                    },
+                    editrules: {required: false, edithidden: true}
+                },
+                {label: 'Source',name: 'sourcetype',width: 100, align: "left",editable: true, edittype: 'select', hidden:true,
+                    editrules: {edithidden: true, required: false},
+                    editoptions: {
+                    value: "Telkom Group:Telkom Group;Telin:Telin",
+                    dataInit: function(elem) {
+                        $(elem).width(150);  // set the width which you need
+                    }
+                }},
+                {label: 'Description',name: 'description',width: 200, align: "left",editable: true, hidden:true,
+                    edittype:'textarea',
+                    editoptions: {
+                        rows: 2,
+                        cols:50,
+                        maxlength:128
+                    }
+                },
+                {label: 'Creation Date',name: 'creationdate',width: 150, hidden: true,  align: "left",editable: true,
+                    editoptions: {
+                        size: 15,
+                        maxlength:10,
+                        defaultValue: 'By System'
+                    },
+                    editrules: {edithidden: true}
+                },
+                {label: 'Created By',name: 'createdby',width: 150, hidden: true,  align: "left",editable: true,
+                    editoptions: {
+                        size: 15,
+                        maxlength:10,
+                        defaultValue: 'By System'
+                    },
+                    editrules: {edithidden: true}
+                },
+                {label: 'Updated Date',name: 'updateddate',width: 150, hidden: true,  align: "left",editable: true,
+                    editoptions: {
+                        size: 15,
+                        maxlength:10,
+                        defaultValue: 'By System'
+                    },
+                    editrules: {edithidden: true}
+                },
+                {label: 'Updated By',name: 'updatedby',width: 150, hidden: true,  align: "left",editable: true,
+                    editoptions: {
+                        size: 15,
+                        maxlength:10,
+                        defaultValue: 'By System'
+                    },
+                    editrules: {edithidden: true}
+                }
+            ],
+            height: '100%',
+            autowidth: true,
+            viewrecords: true,
+            rowNum: 10,
+            rowList: [10,20,50],
+            rownumbers: true, // show row numbers
+            rownumWidth: 35, // the width of the row numbers columns
+            altRows: true,
+            shrinkToFit: true,
+            multiboxonly: true,
+            onSelectRow: function (rowid) {
+                /*do something when selected*/
+
+            },
+            sortorder:'',
+            pager: '#grid-pager',
+            jsonReader: {
+                root: 'rows',
+                id: 'id',
+                repeatitems: false
+            },
+            loadComplete: function (response) {
+                if(response.success == false) {
+                    swal({title: 'Attention', text: response.message, html: true, type: "warning"});
+                }
+
+            },
+            //memanggil controller jqgrid yang ada di controller crud
+            editurl: '<?php echo WS_JQGRID."parameter.tblm_costcenter_controller/crud"; ?>',
+            caption: "Telin Cost Center"
+
+        });
+
+        jQuery('#grid-table').jqGrid('navGrid', '#grid-pager',
+            {   //navbar options
+                edit: true,
+                editicon: 'fa fa-pencil blue bigger-120',
+                add: true,
+                addicon: 'fa fa-plus-circle purple bigger-120',
+                del: true,
+                delicon: 'fa fa-trash-o red bigger-120',
+                search: false,
+                searchicon: 'fa fa-search orange bigger-120',
+                refresh: true,
+                afterRefresh: function () {
+                    // some code here
+                    jQuery("#detailsPlaceholder").hide();
+                },
+
+                refreshicon: 'fa fa-refresh green bigger-120',
+                view: false,
+                viewicon: 'fa fa-search-plus grey bigger-120'
+            },
+
+            {
+                // options for the Edit Dialog
+                closeAfterEdit: true,
+                closeOnEscape:true,
+                recreateForm: true,
+                serializeEditData: serializeJSON,
+                width: 'auto',
+                errorTextFormat: function (data) {
+                    return 'Error: ' + data.responseText
+                },
+                beforeShowForm: function (e, form) {
+                    var form = $(e[0]);
+                    style_edit_form(form);
+
+                    $('#creationdate').attr('readonly', true);
+                    $('#createdby').attr('readonly', true);
+                    $('#updateddate').attr('readonly', true);
+                    $('#updatedby').attr('readonly', true);
+
+                },
+                afterShowForm: function(form) {
+                    form.closest('.ui-jqdialog').center();
+                },
+                afterSubmit:function(response,postdata) {
+                    var response = jQuery.parseJSON(response.responseText);
+                    if(response.success == false) {
+                        return [false,response.message,response.responseText];
+                    }
+                    return [true,"",response.responseText];
+                }
+            },
+            {
+                //new record form
+                closeAfterAdd: false,
+                clearAfterAdd : true,
+                closeOnEscape:true,
+                recreateForm: true,
+                width: 'auto',
+                errorTextFormat: function (data) {
+                    return 'Error: ' + data.responseText
+                },
+                serializeEditData: serializeJSON,
+                viewPagerButtons: false,
+                beforeShowForm: function (e, form) {
+                    var form = $(e[0]);
+                    style_edit_form(form);
+
+                    $('#creationdate').attr('readonly', true);
+                    $('#createdby').attr('readonly', true);
+                    $('#updateddate').attr('readonly', true);
+                    $('#updatedby').attr('readonly', true);
+                },
+                afterShowForm: function(form) {
+                    form.closest('.ui-jqdialog').center();
+                },
+                afterSubmit:function(response,postdata) {
+                    var response = jQuery.parseJSON(response.responseText);
+                    if(response.success == false) {
+                        return [false,response.message,response.responseText];
+                    }
+
+                    $(".tinfo").html('<div class="ui-state-success">' + response.message + '</div>');
+                    var tinfoel = $(".tinfo").show();
+                    tinfoel.delay(3000).fadeOut();
+
+
+                    return [true,"",response.responseText];
+                }
+            },
+            {
+                //delete record form
+                serializeDelData: serializeJSON,
+                recreateForm: true,
+                beforeShowForm: function (e) {
+                    var form = $(e[0]);
+                    style_delete_form(form);
+
+                },
+                afterShowForm: function(form) {
+                    form.closest('.ui-jqdialog').center();
+                },
+                onClick: function (e) {
+                    //alert(1);
+                },
+                afterSubmit:function(response,postdata) {
+                    var response = jQuery.parseJSON(response.responseText);
+                    if(response.success == false) {
+                        return [false,response.message,response.responseText];
+                    }
+                    return [true,"",response.responseText];
+                }
+            },
+            {
+                //search form
+                closeAfterSearch: false,
+                recreateForm: true,
+                afterShowSearch: function (e) {
+                    var form = $(e[0]);
+                    style_search_form(form);
+                    form.closest('.ui-jqdialog').center();
+                },
+                afterRedraw: function () {
+                    style_search_filters($(this));
+                }
+            },
+            {
+                //view record form
+                recreateForm: true,
+                beforeShowForm: function (e) {
+                    var form = $(e[0]);
+                }
+            }
+        );
+
+    });
+
+    function responsive_jqgrid(grid_selector, pager_selector) {
+
+        var parent_column = $(grid_selector).closest('[class*="col-"]');
+        $(grid_selector).jqGrid( 'setGridWidth', $(".page-content").width() );
+        $(pager_selector).jqGrid( 'setGridWidth', parent_column.width() );
+
+    }
+
+</script>
