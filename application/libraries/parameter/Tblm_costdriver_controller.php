ebe537f22ca356d1048465ba0d06f327f605cb85
saepudin06 <saepudin06@gmail.com>
2018-08-05 15:09:20 +0700

    ''

--- application/libraries/parameter/Tblm_costdriver_controller.php
+++ application/libraries/parameter/Tblm_costdriver_controller.php
@@ -90,6 +90,45 @@ class Tblm_costdriver_controller {
         return $data;
     }
 
+    function readLovNew() {
+
+        $start = getVarClean('current','int',0);
+        $limit = getVarClean('rowCount','int',5);
+
+        $sort = getVarClean('sort','str','a.listingno');
+        $dir  = getVarClean('dir','str','asc');
+
+        $searchPhrase = getVarClean('searchPhrase', 'str', '');
+
+        $data = array('rows' => array(), 'success' => false, 'message' => '', 'current' => $start, 'rowCount' => $limit, 'total' => 0);
+
+        try {
+
+            $ci = & get_instance();
+            $ci->load->model('parameter/tblm_costdriver');
+            $table = $ci->tblm_costdriver;
+
+            if(!empty($searchPhrase)) {
+                $table->setCriteria("( upper(a.code) like upper('%".$searchPhrase."%') )");
+
+            }
+
+
+            $start = ($start-1) * $limit;
+            $items = $table->getAll($start, $limit, $sort, $dir);
+            $totalcount = $table->countAll();
+
+            $data['rows'] = $items;
+            $data['success'] = true;
+            $data['total'] = $totalcount;
+
+        }catch (Exception $e) {
+            $data['message'] = $e->getMessage();
+        }
+
+        return $data;
+    }
+
     function readLov() {
 
         $start = getVarClean('current','int',0);
